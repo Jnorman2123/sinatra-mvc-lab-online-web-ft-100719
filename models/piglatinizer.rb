63ac323bbb95f15262390dc6dbad6f85a65eb89a
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:41:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,16 +10,16 @@ class PigLatinizer
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z B C D F G H J K L M N P Q R S T V W X Y Z]
     split_text.each do |word|
       piglatin_words = []
-      if vowels.include?(text[0])
-        text + "way"
-      elsif consonants.include?(text[0]) && consonants.include?(text[1]) && consonants.include?(text[2])
-        text[3..-1] + text[0..2] + "ay"
-      elsif consonants.include?(text[0]) && consonants.include?(text[1])
-        text[2..-1] + text[0..1] + "ay"
-      elsif text[0..1] == "qu"
-        text[2..-1] + text[0..1] + "ay"
-      elsif consonants.include?(text[0])
-        text[1..-1] + text[0] + "ay"
+      if vowels.include?(word[0])
+        word + "way"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        word[3..-1] + word[0..2] + "ay"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        word[2..-1] + word[0..1] + "ay"
+      elsif word[0..1] == "qu"
+        word[2..-1] + word[0..1] + "ay"
+      elsif consonants.include?(word[0])
+        word[1..-1] + word[0] + "ay"
       end
     end
   end
