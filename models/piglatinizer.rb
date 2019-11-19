1a898e980caa1c4db545b0c9473cea90f117bf3b
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:23:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,16 +8,16 @@ class PigLatinizer
     split_text = text.split
     vowels = %w[a e i o u]
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
-      if vowels.include?(text[0])
-        text + "way"
-      elsif consonants.include?(text[0]) && consonants.include?(text[1]) && consonants.include?(text[2])
-        text[3..-1] + text[0..2] + "way"
-      elsif consonants.include?(text[0]) && consonants.include?(text[1])
-        text[2..-1] + text[0..1] + "way"
-      elsif text[0..1] == "qu"
-        text[2..-1] + text[0..1] + "way"
+      if vowels.include?(split_text[0])
+        split_text + "way"
+      elsif consonants.include?(split_text[0]) && consonants.include?(split_text[1]) && consonants.include?(split_text[2])
+        split_text[3..-1] + split_text[0..2] + "way"
+      elsif consonants.include?(split_text[0]) && consonants.include?(split_text[1])
+        split_text[2..-1] + split_text[0..1] + "way"
+      elsif split_text[0..1] == "qu"
+        split_text[2..-1] + split_text[0..1] + "way"
       elsif consonants.include?(@text[0])
-        text[1..-1] + text[0] + "way"
+        split_text[1..-1] + split_text[0] + "way"
       end
       binding.pry
   end
