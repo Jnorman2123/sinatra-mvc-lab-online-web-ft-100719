045c080a4be4d44e3d8fa6163598a66ce4af0531
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:46:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,7 @@ class PigLatinizer
 
   def piglatinize(text)
     split_text = self.splits(text)
+    binding.pry
     vowels = %w[a e i o u A E I O U]
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z B C D F G H J K L M N P Q R S T V W X Y Z]
     split_text.each do |word|
