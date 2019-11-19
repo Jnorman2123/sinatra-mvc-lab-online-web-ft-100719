f9580684677aeda617816ece6fc06a5e19916e17
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:43:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,16 +11,17 @@ class PigLatinizer
     split_text.each do |word|
       piglatin_words = []
       if vowels.include?(word[0])
-        word + "way"
+        piglatin_words << (word + "way")
       elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-        word[3..-1] + word[0..2] + "ay"
+        piglatin_words << (word[3..-1] + word[0..2] + "ay")
       elsif consonants.include?(word[0]) && consonants.include?(word[1])
-        word[2..-1] + word[0..1] + "ay"
+        piglatin_words << (word[2..-1] + word[0..1] + "ay")
       elsif word[0..1] == "qu"
-        word[2..-1] + word[0..1] + "ay"
+        piglatin_words << (word[2..-1] + word[0..1] + "ay")
       elsif consonants.include?(word[0])
-        word[1..-1] + word[0] + "ay"
+        piglatin_words << (word[1..-1] + word[0] + "ay")
       end
+      piglatin_words.join(" ")
     end
   end
 
