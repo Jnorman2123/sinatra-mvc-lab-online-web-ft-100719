726fbc5dffb331492436dbdc76ca0d696a82c80c
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:56:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,14 +8,15 @@ class App < Sinatra::Base
     erb :user_input
   end
 
+  get '/piglatinize' do
+
+    erb :piglatinize
+  end
+
   post '/piglatinize' do
     @translation = PigLatinizer.new(params[:words_to_translate])
 
     redirect "/piglatinize"
   end
 
-  get '/piglatinize' do
-
-    erb :piglatinize
-  end
 end
