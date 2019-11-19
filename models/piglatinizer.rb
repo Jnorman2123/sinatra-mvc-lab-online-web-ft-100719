1d51fe39a0b9d43a0d9e865c066db842de594dde
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:21:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,16 +7,16 @@ class PigLatinizer
   def piglatinize(text)
     vowels = %w[a e i o u]
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
-      if vowels.include?(@text[0])
-        @text + "way"
-      elsif consonants.include?(@text[0]) && consonants.include?(@text[1]) && consonants.include?(@text[2])
-        @text[3..-1] + @text[0..2] + "way"
-      elsif consonants.include?(@text[0]) && consonants.include?(@text[1])
-        @text[2..-1] + @text[0..1] + "way"
-      elsif @text[0..1] == "qu"
-        @text[2..-1] + @text[0..1] + "way"
+      if vowels.include?(text[0])
+        text + "way"
+      elsif consonants.include?(text[0]) && consonants.include?(text[1]) && consonants.include?(text[2])
+        text[3..-1] + text[0..2] + "way"
+      elsif consonants.include?(text[0]) && consonants.include?(text[1])
+        text[2..-1] + text[0..1] + "way"
+      elsif text[0..1] == "qu"
+        text[2..-1] + text[0..1] + "way"
       elsif consonants.include?(@text[0])
-        @text[1..-1] + @text[0] + "way"
+        text[1..-1] + text[0] + "way"
       end
       binding.pry
   end
