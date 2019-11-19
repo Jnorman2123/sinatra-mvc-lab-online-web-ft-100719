08ddc8caa493d072d7f27b6a894025f88d3de22d
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 22:11:01 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,10 +5,6 @@ class PigLatinizer
     @text = text.downcase
   end
 
-  def split_words
-    @words = @text.split
-  end
-
   def translate_to_piglatin
     vowels = %w[a e i o u]
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
